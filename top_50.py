eee02f4781f39b58c7b268264dca383506a2e584
apierce1992 <apierce1992@gmail.com>
2015-09-12 12:06:12 -0400

    Have a top 50 for user

--- top_50.py
+++ top_50.py
@@ -1,18 +1,37 @@
 from movie_lib import *
 
 
-
-def main():
-    print("Okay let's see the top 50 movies! At least according to Movie Lens...")
+def top_50():
     popularity = {}
     for key, value in all_ratings.items():
         if len(value) >= 20:
             avg = sum(value)/len(value)
             popularity[key] = round(avg, 2)
-    sorted_list = sorted(popularity.items(), key = lambda c: c[1], reverse = True)
-    top_50 = sorted_list[:50]
-    counter = 0
+            sorted_list = sorted(popularity.items(), key = lambda c: c[1], reverse = True)
+            top_50 = sorted_list[:50]
+            counter = 0
     for x in top_50:
         print(str(counter+1) + ': ' +str(all_movies[top_50[counter][0]]) + ' ' + str(top_50[counter][1]))
         counter += 1
+
+def top_50_for_user(user_id):
+    pop_unseen = {}
+    some_user = User(user_id)
+    user_seen = some_user.all_seen()
+    for key, value in all_ratings.items():
+        if key not in user_seen:
+            if len(value) >= 20:
+                avg = sum(value)/len(value)
+                pop_unseen[key] = round(avg, 2)
+                sorted_list = sorted(pop_unseen.items(), key = lambda c: c[1], reverse = True)
+                top_50 = sorted_list[:50]
+                counter = 0
+    for x in top_50:
+        print(str(counter+1) + ': ' +str(all_movies[top_50[counter][0]]) + ' ' + str(top_50[counter][1]))
+        counter += 1
+
+
+def main():
+    print("Okay let's see the top 50 movies! At least according to Movie Lens...")
+    top_50_for_user(1)
 main()
